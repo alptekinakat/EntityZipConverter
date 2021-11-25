c42821219e0213596fd2e7f1d5ca1aa035e3aee8
Alptekin Akat <59018836+alptekinakat@users.noreply.github.com>
2021-11-25 15:53:09 +0300

    Add files via upload

---
+++ ZipConverter.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace PersolusDb.Helper
+{
+    public static class ZipConverter
+    {
+        private static byte[] Compress(byte[] data)
+        {
+            if ((data != null) && (data.Length > 1024))
+            {
+                using (MemoryStream output = new MemoryStream())
+                {
+                    using (DeflateStream dstream = new DeflateStream(output, CompressionLevel.Optimal))
+                    {
+                        dstream.Write(data, 0, data.Length);
+                    }
+                    return output.ToArray();
+                }
+            }
+            else
+            {
+                return data;
+            }
+        }
+        private static byte[] Decompress(byte[] data)
+        {
+            if (data != null)
+            {
+                using (MemoryStream input = new MemoryStream(data))
+                using (MemoryStream output = new MemoryStream())
+                {
+                    using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
+                    {
+                        dstream.CopyTo(output);
+                    }
+                    return output.ToArray();
+                }
+            }
+            else
+            {
+                return data;
+            }
+        }
+
+        public static ValueConverter<byte[], byte[]> ZipliAlan = new ValueConverter<byte[], byte[]>(
+           v => Compress(v),
+           v => Decompress(v));
+    }
+}
